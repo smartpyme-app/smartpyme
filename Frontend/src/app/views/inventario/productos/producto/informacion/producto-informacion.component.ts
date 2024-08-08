90871b789f84c4cef3645a99602831d894408ff8
ajla <josephelop07@gmail.com>
2024-08-08 08:33:43 -0600

    Modulo: Inventario - Creacion de productos v2
    Creación de nuevos fields para productos

--- Frontend/src/app/views/inventario/productos/producto/informacion/producto-informacion.component.ts
+++ Frontend/src/app/views/inventario/productos/producto/informacion/producto-informacion.component.ts
@@ -14,6 +14,7 @@ export class ProductoInformacionComponent implements OnInit {
 
     @Input() producto: any = {};
     public categorias:any = [];
+    public proveedores:any = [];
     public usuario:any = {};
     public categoria:any = {};
     public bodegas:any = [];
@@ -36,6 +37,11 @@ export class ProductoInformacionComponent implements OnInit {
         }, error => {this.alertService.error(error);});
 
         this.medidas = JSON.parse(localStorage.getItem('unidades_medidas')!);
+        
+        this.apiService.getAll('proveedores/list').subscribe(proveedores => {
+            this.proveedores = proveedores;
+            this.loading = false;
+        }, error => {this.alertService.error(error); this.loading = false;});
 
     }
 
@@ -109,6 +115,24 @@ export class ProductoInformacionComponent implements OnInit {
             }, error => {this.alertService.error(error); this.loading = false;});
         }
     }
+
+    // creacion de sku 
+
+    public createSku(detalle:any){
+        if(!detalle.cantidad){
+            detalle.cantidad = 0;
+        }
+        if(detalle.descuento_porcentaje){
+            detalle.descuento = detalle.cantidad * (detalle.precio * (detalle.descuento_porcentaje / 100));
+        }else{
+            detalle.descuento = 0;
+        }
+
+        detalle.total_costo  = (parseFloat(detalle.cantidad) * parseFloat(detalle.costo)).toFixed(4);
+        detalle.total  = (parseFloat(detalle.cantidad) * parseFloat(detalle.precio) - parseFloat(detalle.descuento)).toFixed(4);
+        // this.update.emit(this.venta);
+    }
+
 
     
 
