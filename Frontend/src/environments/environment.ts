99138782858286949098597a4fa3666a523fa66e
Jesús Alvarado <alvarado.websis@gmail.com>
2024-08-13 19:25:03 -0600

    Enviroment: Update

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   API_URL: 'http://localhost:8000',
   // API_URL: 'https://api.smartpyme.site',
-  // API_URL: 'https://apitest.smartpyme.site',
+  // API_URL: 'https://apiconta.smartpyme.site',
   APP_URL: 'http://localhost:4200',
 };
 
