cfa1a982828ae56b3e592b82f248ae4b26d37415
alexis-garcia-dev <alexis.garcia170698@gmail.com>
2025-01-06 12:31:40 -0600

    Enhance NotificacionesController and Frontend Notification Options
    - Added logging functionality to NotificacionesController for improved debugging and monitoring of notification requests.
    - Updated the notificaciones route to ensure proper handling of notification requests.
    - Expanded the frontend notification dropdown to include "Orden de Producción," allowing users to filter notifications by this new type.
    
    These changes improve the notification system's functionality and user experience by providing additional options and better tracking of notification processes.

--- Backend/routes/modulos/admin/notificaciones.php
+++ Backend/routes/modulos/admin/notificaciones.php
@@ -1,6 +1,7 @@
 <?php 
 
 use App\Http\Controllers\Api\Admin\NotificacionesController;
+use Illuminate\Support\Facades\Route;
 
     Route::get('/notificaciones',                  [NotificacionesController::class, 'index']);
     Route::get('/notificacion/{id}',              [NotificacionesController::class, 'read']);
