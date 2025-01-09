c395ffce026b60b6e09fa4dcf0d0f4b2df7c28ac
Jesús Alvarado <alvarado.websis@gmail.com>
2025-01-09 17:03:20 +0000

    Primer cambio citas
    (cherry picked from commit f57a5f550e6b484c43d3adbb3d1017541bdbbe3f)
    
    Co-authored-by: Edgar Avelar <edgareeel@gmail.com>

--- Frontend/src/app/views/citas/calendario/calendario.component.ts
+++ Frontend/src/app/views/citas/calendario/calendario.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,TemplateRef, ViewChild, forwardRef, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild, forwardRef, Output, EventEmitter } from '@angular/core';
 import { CalendarOptions, Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
@@ -19,162 +19,216 @@ import * as moment from 'moment';
 
 @Component({
   selector: 'app-calendario',
-  templateUrl: './calendario.component.html'
+  templateUrl: './calendario.component.html',
+  styleUrls: ['./calendario.component.scss']
 })
 export class CalendarioComponent implements OnInit {
 
-    @Output() update = new EventEmitter();
-    public eventos:any = [];
-    public evento:any = {};
-    public filtros:any = {};
-    public loading:boolean = false;
-    public saving:boolean = false;
-    public calendarOptions?: CalendarOptions;
-    eventsModel: any;
+  @Output() update = new EventEmitter();
+  public eventos: any = [];
+  public evento: any = {};
+  public filtros: any = {};
+  public loading: boolean = false;
+  public saving: boolean = false;
+  public calendarOptions?: CalendarOptions;
+  eventsModel: any;
 
-    @ViewChild('mevento')
-    public meventoTemplate!: TemplateRef<any>;
-    modalRef!: BsModalRef;
+  @ViewChild('mevento')
+  public meventoTemplate!: TemplateRef<any>;
+  modalRef!: BsModalRef;
+  selectedPeriodType: "day" | "week" | "month" | "year" = "day";
+  constructor(private apiService: ApiService, public alertService: AlertService,
+    private route: ActivatedRoute, private router: Router,
+    private modalService: BsModalService
+  ) { }
 
-    constructor(private apiService: ApiService, public alertService: AlertService,  
-        private route: ActivatedRoute, private router: Router,
-        private modalService: BsModalService
-    ){ }
+  @ViewChild('fullcalendar') fullcalendar?: FullCalendarComponent;
+  get calendar(): Calendar | undefined {
+    return this.fullcalendar?.getApi();
+  }
+  ngOnInit() {
+    forwardRef(() => Calendar);
 
-    @ViewChild('fullcalendar') fullcalendar?: FullCalendarComponent;
-
-    ngOnInit() {
-        forwardRef(() => Calendar);
-
-        this.calendarOptions = {
-            // plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin, multiMonthPlugin],
-            plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, multiMonthPlugin, rrulePlugin],
-            editable: true,
-            navLinks: true,
-            firstDay: 0,
-            timeZone: 'America/El_Salvador',
-            locale: esLocale,
-            themeSystem: 'bootstrap',
-            businessHours: [ // specify an array instead
-              {
-                daysOfWeek: [ 1, 2, 3, 4, 5 ], // Monday, Tuesday, Wednesday
-                startTime: '08:00', // 8am
-                endTime: '17:00' // 5pm
-              },
-              {
-                daysOfWeek: [ 6 ], // Thursday, Friday
-                startTime: '08:00', // 10am
-                endTime: '12:00' // 4pm
-              }
-            ],
-            headerToolbar: {
-              left: 'prev,next today',
-              center: 'title',
-              right: 'dayGridMonth,timeGridWeek,timeGridDay,multiMonthYear,listWeek'
-            },
-            customButtons: {
-              myCustomButton: {
-                text: 'Nuevo',
-                click: this.handleDateClick.bind(this)
-              }
-            },
-            dateClick: this.handleDateClick.bind(this),
-            eventClick: this.handleEventClick.bind(this),
-            eventChange: this.handleEventChange.bind(this),
-            events: []
-        };
-
-        this.loadAll();
-    }
-
-    public loadAll(){
-        this.filtros.id_sucursal = this.apiService.auth_user().id_sucursal;
-        this.filtros.orden = 'inicio';
-        this.filtros.direccion = 'desc';
-        this.loading = true;
-        this.apiService.getAll('eventos/list', this.filtros).subscribe(eventos => { 
-            this.loading = false;
-            if(this.calendarOptions){
-                this.calendarOptions.events = eventos;
-            }
-            if(this.modalRef){
-                this.modalRef.hide();
-            }
-            console.log('siu');
-            this.update.emit();
-        }, error => {this.alertService.error(error); this.loading = false;});
-    }
-
-    handleDateClick(arg:any) {
-        this.evento = {};
-        this.evento.frecuencia = '';
-        this.evento.tipo = 'Sin confirmar';
-        this.evento.duracion = "1 hora";
-        this.evento.estado = "Activo";
-        this.evento.id_cliente = '';
-        this.evento.id_servicio = '';
-        this.evento.productos = [];
-        this.evento.id_empresa = this.apiService.auth_user().id_empresa;
-        this.evento.id_usuario = this.apiService.auth_user().id;
-        this.evento.id_sucursal = this.apiService.auth_user().id_sucursal;
-        this.evento.inicio =  moment(arg.dateStr + ' ' + moment().format('HH:mm')).format('YYYY-MM-DD HH:mm:ss');
-        this.setTime();
-        this.alertService.modal = true;
-        this.modalRef = this.modalService.show(this.meventoTemplate, {class: 'modal-lg'});
-    }
-
-
-    handleEventClick(arg:any) {
-        this.evento = arg.event.extendedProps.data;
-        this.alertService.modal = true;
-        this.modalRef = this.modalService.show(this.meventoTemplate, {class: 'modal-lg'});
-    }
-
-    setTime(){
-        let fecha = moment(this.evento.inicio);
-
-        if(this.evento.duracion == '15 minutos'){
-            this.evento.fin = fecha.add(15, 'minutes').format('YYYY-MM-DD HH:mm');
+    this.calendarOptions = {
+      // plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin, multiMonthPlugin],
+      plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, multiMonthPlugin, rrulePlugin],
+      editable: true,
+      navLinks: true,
+      firstDay: 0,
+      timeZone: 'America/El_Salvador',
+      locale: esLocale,
+      // themeSystem: 'bootstrap5',
+      themeSystem: 'bootstrap',
+      businessHours: [ // specify an array instead
+        {
+          daysOfWeek: [1, 2, 3, 4, 5], // Monday, Tuesday, Wednesday
+          startTime: '08:00', // 8am
+          endTime: '17:00' // 5pm
+        },
+        {
+          daysOfWeek: [6], // Thursday, Friday
+          startTime: '08:00', // 10am
+          endTime: '12:00' // 4pm
         }
-        if(this.evento.duracion == '30 minutos'){
-            this.evento.fin = fecha.add(30, 'minutes').format('YYYY-MM-DD HH:mm');
+      ],
+      headerToolbar: {
+        left: '',
+        center: '',
+        right: ''
+      },
+      customButtons: {
+        myCustomButton: {
+          text: 'Nuevo',
+          click: this.handleDateClick.bind(this)
         }
-        if(this.evento.duracion == '1 hora'){
-            this.evento.fin = fecha.add(1, 'hour').format('YYYY-MM-DD HH:mm');
+      },
+      initialView: 'timeGridDay',
+      views: {
+        timeGridDay: {
+          slotLabelFormat: {
+            hour: 'numeric',
+            minute: '2-digit',
+            omitZeroMinute: false,
+            meridiem: 'short'
+          }
         }
-        if(this.evento.duracion == '2 horas'){
-            this.evento.fin = fecha.add(2, 'hour').format('YYYY-MM-DD HH:mm');
-        }
-        if(this.evento.duracion == '3 horas'){
-            this.evento.fin = fecha.add(3, 'hour').format('YYYY-MM-DD HH:mm');
-        }
-        if(this.evento.duracion == '5 horas'){
-            this.evento.fin = fecha.add(5, 'hour').format('YYYY-MM-DD HH:mm');
-        }
-    }
+      },
+      eventContent: function (arg) {
+        let italicEl = document.createElement('span')
+        italicEl.classList.add('event-container');
 
-    handleEventChange(arg:any) {
-        this.evento = arg.event.extendedProps.data;
-        this.evento.inicio = moment(arg.event.start).format('YYYY-MM-DD HH:mm');
-        this.setTime();
-        console.log(this.evento);
-        this.onSubmit();
-    }
+        italicEl.innerHTML = `
+        <span class="d-flex justify-content-between event-title">
+            <span><i class="fa fa-bell"></i> ${arg.event.title}</span>
+            <span>${arg.timeText}</span>
+        </span>
+        <span class="w-100 event-body">
+            ${arg.event.extendedProps['data'].descripcion.slice(0, 50)}...
+        </span>`;
+        let event = arg.event.extendedProps['data'];
+        console.log(event);
 
-    public onSubmit(){
-        this.saving = true;
-        this.apiService.store('evento', this.evento).subscribe(evento => {
-            if (!this.evento.id) {
-                this.alertService.success('Cita creada', 'La cita fue añadida exitosamente.');
-            }else{
-                this.alertService.success('Cita guardada', 'La cita fue guardada exitosamente.');
-            }
-            this.loadAll();
-            this.saving = false;
-            this.modalRef.hide();
-            this.alertService.modal = false;
-        }, error => {this.alertService.error(error); this.saving = false;});
-    }
+        // if (arg.event.extendedProps) {
+        // } else {
+        //   italicEl.innerHTML = 'normal event'
+        // }
 
+        let arrayOfDomNodes = [italicEl]
+        return { domNodes: arrayOfDomNodes }
+      },
+      dateClick: this.handleDateClick.bind(this),
+      eventClick: this.handleEventClick.bind(this),
+      eventChange: this.handleEventChange.bind(this),
+      events: []
+    };
+
+    this.loadAll();
+  }
+
+  public loadAll() {
+    this.filtros.id_sucursal = this.apiService.auth_user().id_sucursal;
+    this.filtros.orden = 'inicio';
+    this.filtros.direccion = 'desc';
+    this.loading = true;
+    this.apiService.getAll('eventos/list', this.filtros).subscribe(eventos => {
+      this.loading = false;
+      if (this.calendarOptions) {
+        this.calendarOptions.events = eventos;
+      }
+      if (this.modalRef) {
+        this.modalRef.hide();
+      }
+      this.update.emit();
+    }, error => { this.alertService.error(error); this.loading = false; });
+  }
+
+  handleDateClick(arg: any) {
+    this.evento = {};
+    this.evento.frecuencia = '';
+    this.evento.tipo = 'Sin confirmar';
+    this.evento.duracion = "1 hora";
+    this.evento.estado = "Activo";
+    this.evento.id_cliente = '';
+    this.evento.id_servicio = '';
+    this.evento.productos = [];
+    this.evento.id_empresa = this.apiService.auth_user().id_empresa;
+    this.evento.id_usuario = this.apiService.auth_user().id;
+    this.evento.id_sucursal = this.apiService.auth_user().id_sucursal;
+    this.evento.inicio = moment(arg.dateStr + ' ' + moment().format('HH:mm')).format('YYYY-MM-DD HH:mm:ss');
+    this.setTime();
+    this.alertService.modal = true;
+    this.modalRef = this.modalService.show(this.meventoTemplate, { class: 'modal-lg' });
+  }
+
+
+  handleEventClick(arg: any) {
+    this.evento = arg.event.extendedProps.data;
+    this.alertService.modal = true;
+    this.modalRef = this.modalService.show(this.meventoTemplate, { class: 'modal-lg' });
+  }
+
+  setTime() {
+    let fecha = moment(this.evento.inicio);
+
+    if (this.evento.duracion == '15 minutos') {
+      this.evento.fin = fecha.add(15, 'minutes').format('YYYY-MM-DD HH:mm');
+    }
+    if (this.evento.duracion == '30 minutos') {
+      this.evento.fin = fecha.add(30, 'minutes').format('YYYY-MM-DD HH:mm');
+    }
+    if (this.evento.duracion == '1 hora') {
+      this.evento.fin = fecha.add(1, 'hour').format('YYYY-MM-DD HH:mm');
+    }
+    if (this.evento.duracion == '2 horas') {
+      this.evento.fin = fecha.add(2, 'hour').format('YYYY-MM-DD HH:mm');
+    }
+    if (this.evento.duracion == '3 horas') {
+      this.evento.fin = fecha.add(3, 'hour').format('YYYY-MM-DD HH:mm');
+    }
+    if (this.evento.duracion == '5 horas') {
+      this.evento.fin = fecha.add(5, 'hour').format('YYYY-MM-DD HH:mm');
+    }
+  }
+
+  handleEventChange(arg: any) {
+    this.evento = arg.event.extendedProps.data;
+    this.evento.inicio = moment(arg.event.start).format('YYYY-MM-DD HH:mm');
+    this.setTime();
+    console.log(this.evento);
+    this.onSubmit();
+  }
+
+  public onSubmit() {
+    this.saving = true;
+    this.apiService.store('evento', this.evento).subscribe(evento => {
+      if (!this.evento.id) {
+        this.alertService.success('Cita creada', 'La cita fue añadida exitosamente.');
+      } else {
+        this.alertService.success('Cita guardada', 'La cita fue guardada exitosamente.');
+      }
+      this.loadAll();
+      this.saving = false;
+      this.modalRef.hide();
+      this.alertService.modal = false;
+    }, error => { this.alertService.error(error); this.saving = false; });
+  }
+  setNowSelection() {
+    this.selectedPeriodType = "day";
+    this.calendar?.changeView('timeGridDay');
+  }
+  setMonthSelection() {
+    this.selectedPeriodType = "month";
+    this.calendar?.changeView('dayGridMonth');
+
+  }
+  setWeekSelection() {
+    this.selectedPeriodType = "week";
+    this.calendar?.changeView('dayGridWeek');
+
+  }
+  setYearSelection() {
+    this.selectedPeriodType = "year";
+    this.calendar?.changeView('multiMonth');
+  }
 
 }
