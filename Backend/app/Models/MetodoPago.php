67f96ba4ffce8c7f3e927b878da6ef05caf631a4
José España <joseespana94@gmail.com>
2025-01-22 17:00:28 -0600

    Update MetodoPago.php

--- Backend/app/Models/MetodoPago.php
+++ Backend/app/Models/MetodoPago.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class MetodoPago extends Model
 {
     use HasFactory;
+
+    protected $table = 'metodos_pago';
+    protected $fillable = [
+        'id_usuario',
+        'id_tarjeta',
+        'marca_tarjeta',
+        'ultimos_cuatro',
+        'titular_tarjeta',
+        'nombre_emisor',
+        'codigo_pais',
+        'codigo_estado',
+        'codigo_postal',
+        'es_predeterminado',
+        'esta_activo',
+    ];
 }
