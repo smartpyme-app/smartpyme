12c0be3014e572b401068f615691779f32e3d595
alexis-garcia-dev <alexis.garcia170698@gmail.com>
2025-02-05 17:05:05 -0600

    Add dynamic column count calculation and clean up ProductoSpecsComponent

--- Frontend/src/app/views/ventas/facturacion/facturacion-tienda/detalles/producto-specs.component.ts
+++ Frontend/src/app/views/ventas/facturacion/facturacion-tienda/detalles/producto-specs.component.ts
@@ -1,16 +1,15 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-product-specs',
+  selector: 'app-producto-specs',
   template: `
     <ng-container [ngSwitch]="field.field_type">
-      <select *ngSwitchCase="'select'"
+      <select *ngSwitchCase="'select'" 
               class="form-select form-select-sm"
               [(ngModel)]="selectedValue"
               (change)="specChanged()">
         <option value="">Seleccionar</option>
-        <option *ngFor="let value of field.values" 
-                [ngValue]="value">
+        <option *ngFor="let value of field.values" [ngValue]="value">
           {{value.value}}
         </option>
       </select>
@@ -20,57 +19,37 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
              class="form-control form-control-sm"
              [(ngModel)]="inputValue"
              (change)="specChanged()">
+
+      <input *ngSwitchCase="'number'"
+             type="number"
+             class="form-control form-control-sm" 
+             [(ngModel)]="inputValue"
+             (change)="specChanged()">
     </ng-container>
   `
 })
-export class ProductoSpecsComponent implements OnInit {
+export class ProductoSpecsComponent {
   @Input() producto: any;
   @Input() field: any;
   @Output() onChange = new EventEmitter();
 
-  selectedValue: any = null;
+  selectedValue: any;
   inputValue: string | number = '';
 
   ngOnInit() {
-    if (!this.producto.custom_fields) {
+    if(!this.producto.custom_fields) {
       this.producto.custom_fields = [];
-    }
-
-
-    const existingField = this.producto.custom_fields.find(
-      (cf: any) => cf.custom_field?.id === this.field.id
-    );
-
-    if (existingField) {
-      if (this.field.field_type === 'select') {
-
-        this.selectedValue = this.field.values?.find(
-          (v: any) => v.id === existingField.custom_field_value_id
-        );
-      } else {
-        this.inputValue = existingField.value || '';
-      }
     }
   }
 
   specChanged() {
     const customField = {
-
-      id: this.findExistingFieldId(),
-
-      custom_field_id: this.field.id,
-      cotizacion_venta_detalle_id: this.producto.id,
-      value: this.field.field_type === 'select' ? this.selectedValue?.value : this.inputValue,
-      custom_field_value_id: this.field.field_type === 'select' ? this.selectedValue?.id : null,
-
-      custom_field: this.field,
-      custom_field_value: this.selectedValue
+      id: this.field.id,
+      value: this.field.field_type === 'select' ? this.selectedValue.value : this.inputValue,
+      id_value: this.field.field_type === 'select' ? this.selectedValue.id : null
     };
 
-    const index = this.producto.custom_fields.findIndex(
-      (cf: any) => cf.custom_field?.id === this.field.id
-    );
-
+    const index = this.producto.custom_fields.findIndex((cf: any) => cf.id === this.field.id);
     if (index >= 0) {
       this.producto.custom_fields[index] = customField;
     } else {
@@ -78,12 +57,5 @@ export class ProductoSpecsComponent implements OnInit {
     }
 
     this.onChange.emit();
-  }
-
-  private findExistingFieldId(): number | null {
-    const existingField = this.producto.custom_fields.find(
-      (cf: any) => cf.custom_field?.id === this.field.id
-    );
-    return existingField ? existingField.id : null;
   }
 }
