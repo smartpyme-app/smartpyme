77c23b35edbd411375648feab34ddd87a7f5a762
alexis-garcia-dev <alexis.garcia170698@gmail.com>
2025-02-05 20:26:46 -0600

    Refactor ProductoSpecsComponent to initialize custom fields and improve spec change handling

--- Frontend/src/app/views/ventas/facturacion/facturacion-tienda/detalles/producto-specs.component.ts
+++ Frontend/src/app/views/ventas/facturacion/facturacion-tienda/detalles/producto-specs.component.ts
@@ -1,15 +1,16 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-producto-specs',
   template: `
     <ng-container [ngSwitch]="field.field_type">
-      <select *ngSwitchCase="'select'" 
+      <select *ngSwitchCase="'select'"
               class="form-select form-select-sm"
               [(ngModel)]="selectedValue"
               (change)="specChanged()">
         <option value="">Seleccionar</option>
-        <option *ngFor="let value of field.values" [ngValue]="value">
+        <option *ngFor="let value of field.values" 
+                [ngValue]="value">
           {{value.value}}
         </option>
       </select>
@@ -19,37 +20,66 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
              class="form-control form-control-sm"
              [(ngModel)]="inputValue"
              (change)="specChanged()">
-
       <input *ngSwitchCase="'number'"
-             type="number"
-             class="form-control form-control-sm" 
-             [(ngModel)]="inputValue"
-             (change)="specChanged()">
+              type="number" 
+              class="form-control form-control-sm"
+              [(ngModel)]="inputValue"
+              (change)="specChanged()">
+              
     </ng-container>
   `
 })
-export class ProductoSpecsComponent {
+export class ProductoSpecsComponent implements OnInit {
   @Input() producto: any;
   @Input() field: any;
   @Output() onChange = new EventEmitter();
 
-  selectedValue: any;
+  selectedValue: any = null;
   inputValue: string | number = '';
 
   ngOnInit() {
-    if(!this.producto.custom_fields) {
+    //console.log('producto', this.producto);
+    if (!this.producto.custom_fields) {
       this.producto.custom_fields = [];
+    }
+
+
+    const existingField = this.producto.custom_fields.find(
+      (cf: any) => cf.custom_field?.id === this.field.id
+    );
+
+    console.log('existingField', existingField);
+
+    if (existingField) {
+      if (this.field.field_type === 'select') {
+
+        this.selectedValue = this.field.values?.find(
+          (v: any) => v.id === existingField.custom_field_value_id
+        );
+      } else {
+        this.inputValue = existingField.value || '';
+      }
     }
   }
 
   specChanged() {
     const customField = {
-      id: this.field.id,
-      value: this.field.field_type === 'select' ? this.selectedValue.value : this.inputValue,
-      id_value: this.field.field_type === 'select' ? this.selectedValue.id : null
+
+      id: this.findExistingFieldId(),
+
+      custom_field_id: this.field.id,
+      cotizacion_venta_detalle_id: this.producto.id,
+      value: this.field.field_type === 'select' ? this.selectedValue?.value : this.inputValue,
+      custom_field_value_id: this.field.field_type === 'select' ? this.selectedValue?.id : null,
+
+      custom_field: this.field,
+      custom_field_value: this.selectedValue
     };
 
-    const index = this.producto.custom_fields.findIndex((cf: any) => cf.id === this.field.id);
+    const index = this.producto.custom_fields.findIndex(
+      (cf: any) => cf.custom_field?.id === this.field.id
+    );
+
     if (index >= 0) {
       this.producto.custom_fields[index] = customField;
     } else {
@@ -57,5 +87,12 @@ export class ProductoSpecsComponent {
     }
 
     this.onChange.emit();
+  }
+
+  private findExistingFieldId(): number | null {
+    const existingField = this.producto.custom_fields.find(
+      (cf: any) => cf.custom_field?.id === this.field.id
+    );
+    return existingField ? existingField.id : null;
   }
 }
