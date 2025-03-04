069bdff9ef0d2079e380ea7326f6e87ff735ec00
alexis-garcia-dev <alexis.garcia170698@gmail.com>
2025-03-04 15:48:41 -0600

    filtros

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false, 
-  API_URL: 'http://localhost:8000',
+  // API_URL: 'http://localhost:8000',
   // API_URL: 'https://api.smartpyme.site',
-  //API_URL: 'https://apitest.smartpyme.site',
+  API_URL: 'https://apitest.smartpyme.site',
   APP_URL: 'http://localhost:4200',
 };
 
