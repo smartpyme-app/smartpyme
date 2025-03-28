7a32fc9b9da85bae40165f5665cc830df42b3a52
David Camacho <dcamacho@growthaccelerationpartners.com>
2025-03-28 10:43:43 -0600

    Edit AppServiceProvider

--- Backend/app/Providers/AppServiceProvider.php
+++ Backend/app/Providers/AppServiceProvider.php
@@ -2,14 +2,13 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
 use App\Models\Inventario\Inventario;
 use App\Models\Inventario\Producto;
 use App\Observers\InventarioObserver;
 use App\Observers\ProductoObserver;
-use App\Services\AIService;
-use App\Services\ContextService;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,13 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton(ContextService::class, function ($app) {
-            return new ContextService();
-        });
-
-        $this->app->singleton(AIService::class, function ($app) {
-            return new AIService();
-        });
+        //
     }
 
     /**
@@ -36,12 +29,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Registrar el observer de Inventario
         Inventario::observe(InventarioObserver::class);
-        Producto::observe(ProductoObserver::class);
+
+        // Registrar el observer de Producto solo cuando sea necesario
+        if (config('services.woocommerce.enabled', false)) {
+            Producto::observe(ProductoObserver::class);
+        }
+
+        // Registra este scope nuevamente para asegurarte de que se aplique después del observer
+        Producto::addGlobalScope('empresa', function ($builder) {
+            if (Auth::check()) {
+                $builder->where('id_empresa', Auth::user()->id_empresa);
+            }
+        });
+
         Schema::defaultStringLength(191);
         \Carbon\Carbon::setLocale(config('app.locale'));
-        setlocale(LC_ALL,'es_ES.UTF8');
-        setlocale (LC_TIME,'es_ES.UTF8');
-
+        setlocale(LC_ALL, 'es_ES.UTF8');
+        setlocale(LC_TIME, 'es_ES.UTF8');
     }
 }
