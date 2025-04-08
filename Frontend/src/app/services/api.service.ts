bb58beca09734dd4793173be51f8a7c944369f56
alexis-garcia-dev <alexis.garcia170698@gmail.com>
2025-04-08 17:19:48 -0600

    filtros en ventas recurrentes

--- Frontend/src/app/services/api.service.ts
+++ Frontend/src/app/services/api.service.ts
@@ -42,8 +42,7 @@ export class ApiService {
         console.log('filtros x', filtros);
         return this.http.get(this.apiUrl + url , { responseType: 'blob', params: filtros });
     }
-
-    // En el ApiService
+    
     exportAcumulado(url: string, filtros: any): Observable<Blob> {
         console.log('Enviando filtros:', filtros);
         return this.http.post(this.apiUrl + url, filtros, {
