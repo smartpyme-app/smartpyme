3ae820c48be69981168ba4aec0a159c0073acad0
alexis-garcia-dev <alexis.garcia170698@gmail.com>
2025-04-11 14:16:20 -0600

    Refactor currency formatting logic to improve readability and maintainability

--- Frontend/src/app/pipes/currency-format.pipe.ts
+++ Frontend/src/app/pipes/currency-format.pipe.ts
@@ -9,70 +9,41 @@ export class CurrencyPipe implements PipeTransform {
     public currencyCode:any = 'USD';
 
     constructor(private apiService: ApiService) { }
-
-    // transform(value: number): string {
-    //     // Si el valor es nulo o indefinido, se asigna 0.
-    //     if (value === null || value === undefined) {
-    //       value = 0;
-    //     }
-
-    //     // Obtener el código de moneda según el usuario autenticado
-    //     this.currencyCode = this.apiService.auth_user()
-    //       ? this.apiService.auth_user().empresa.moneda
-    //       : 'USD'; // Default a USD si no hay usuario autenticado.
-
-    //     // Si el valor es negativo, formatear y ponerlo entre paréntesis
-    //     if (value < 0) {
-    //       return `(${new Intl.NumberFormat('en-US', {
-    //         style: 'currency',
-    //         currency: this.currencyCode,
-    //       }).format(Math.abs(value))})`;
-    //     }
-
-    //     // Formatear el valor como moneda usando Intl.NumberFormat
-    //     const formattedValue = new Intl.NumberFormat('en-US', {
-    //       style: 'currency',
-    //       currency: this.currencyCode,
-    //     }).format(value);
-
-    //     // Devolver el valor formateado
-    //     return formattedValue;
-    // }
     transform(value: number): string {
-      // Si el valor es nulo o indefinido, se asigna 0.
+     
       if (value === null || value === undefined) {
         value = 0;
       }
     
-      // Obtener datos de moneda desde el usuario autenticado
+      
       const empresa = this.apiService.auth_user()?.empresa;
       this.currencyCode = empresa?.moneda || 'USD';
       
-      // Si tenemos acceso al objeto currency con el símbolo, lo usamos
+      
       const currencySymbol = empresa?.currency?.currency_symbol;
     
-      // Opciones para el formateador
+   
       const options: Intl.NumberFormatOptions = {
         style: 'currency',
         currency: this.currencyCode,
       };
     
-      // Si tenemos un símbolo personalizado, usamos 'decimal' y añadimos el símbolo manualmente
+      
       if (currencySymbol) {
         options.style = 'decimal';
         options.minimumFractionDigits = 2;
         options.maximumFractionDigits = 2;
       }
     
-      // Formatear el valor
+    
       let formattedValue = new Intl.NumberFormat('en-US', options).format(Math.abs(value));
     
-      // Añadir el símbolo personalizado si está disponible
+     
       if (currencySymbol) {
         formattedValue = `${currencySymbol} ${formattedValue}`;
       }
     
-      // Para valores negativos
+      
       if (value < 0) {
         return `(${formattedValue})`;
       }
