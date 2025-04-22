238f012d36fba9b867ec184495d799316537013f
Jesús Alvarado <alvarado.websis@gmail.com>
2025-04-22 16:56:32 -0600

    Se agrego el buscador y se quito la carga completa de clientes

--- Frontend/src/app/shared/parts/buscador-clientes/buscador-clientes.component.ts
+++ Frontend/src/app/shared/parts/buscador-clientes/buscador-clientes.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
-import { AlertService } from '../../../services/alert.service';
-import { ApiService } from '../../../services/api.service';
-
-import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
-import { fromEvent, timer } from 'rxjs';
+import { AlertService } from '@services/alert.service';
+import { ApiService } from '@services/api.service';
+import { fromEvent } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-buscador-clientes',
@@ -11,37 +10,51 @@ import { fromEvent, timer } from 'rxjs';
 })
 export class BuscadorClientesComponent implements OnInit {
 
-	@Output() setCliente = new EventEmitter();
-    @Input() cliente: any = {};
+    @Input() cliente:any = {};
+    @Output() selectCliente = new EventEmitter();
+    @Input() focus:boolean = false;
 
-	public clientes: any = [];
+    public clientes:any = [];
     public searching = false;
+    public searchExecuted: boolean = false;
 
-	constructor(private apiService: ApiService, private alertService: AlertService) { }
+    constructor(private apiService: ApiService, private alertService: AlertService) { }
 
-	ngOnInit() {
-
-        const input = document.getElementById('example')!;
-        const example = fromEvent(input, 'keyup').pipe(map(i => (<HTMLTextAreaElement>i.currentTarget).value));
-        const debouncedInput = example.pipe(debounceTime(500));
-        const subscribe = debouncedInput.subscribe(val => { this.searchProducto(); });
-
+    ngOnInit() {
+        if(!this.cliente.nombre){
+            this.cliente = {};
+        }
+        const input = document.getElementById('buscadorCliente') as HTMLInputElement;
+        fromEvent(input, 'keyup').pipe(
+            map((event: Event) => (event.target as HTMLInputElement).value.trim()), // Obtiene el valor y elimina espacios en blanco
+            debounceTime(800), // Espera 500ms después de la última tecla presionada
+            distinctUntilChanged(), // Evita búsquedas repetidas con el mismo valor
+            filter(value => value.length > 2) // Solo permite búsquedas si hay más de 2 caracteres
+        ).subscribe(() => this.searchClientes());
     }
 
-    searchProducto(){
-        if(this.cliente.nombre && this.cliente.nombre.length > 1) {
-            this.searching = true;
-            this.apiService.read('clientes/buscar/', this.cliente.nombre).subscribe(clientes => {
-               this.clientes = clientes;
-               this.searching = false;
-            }, error => {this.alertService.error(error);this.searching = false;});
-        }else if (!this.cliente.nombre  || this.cliente.nombre.length < 1 ){ this.searching = false; this.cliente = {}; this.clientes.total = 0; }
+
+    searchClientes() {
+        this.searching = true;
+        this.searchExecuted = true;
+        this.apiService.read('clientes/buscar/', this.cliente.nombre).subscribe(clientes => {
+            this.clientes = clientes;
+            this.searching = false;
+        }, error => {
+            this.alertService.error(error);
+            this.searching = false;
+        });
     }
 
-    selectCliente(cliente:any){
+
+    clienteSelect(cliente:any){
+        if(cliente.nombre == ''){
+            cliente = {};
+        }
+        this.searchExecuted = false;
         this.clientes = [];
         this.cliente = cliente;
-        this.setCliente.emit(this.cliente);
+        this.selectCliente.emit(cliente);
     }
 
 }
