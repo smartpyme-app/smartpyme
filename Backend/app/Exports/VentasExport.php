78a60ec9bbc24f2720f88db52693d75a53312d3b
alexis-garcia-dev <alexis.garcia170698@gmail.com>
2025-04-28 16:07:51 -0600

    Refactor VentasExport class for improved readability and added handling for 'Anulada' state in mapping

--- Backend/app/Exports/VentasExport.php
+++ Backend/app/Exports/VentasExport.php
@@ -12,8 +12,8 @@ use Illuminate\Http\Request;
 class VentasExport implements FromCollection, WithHeadings, WithMapping
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public $request;
 
     public function filter(Request $request)
@@ -21,8 +21,9 @@ class VentasExport implements FromCollection, WithHeadings, WithMapping
         $this->request = $request;
     }
 
-    public function headings():array{
-        return[
+    public function headings(): array
+    {
+        return [
             'Fecha',
             'Cliente',
             'Telefono',
@@ -51,95 +52,95 @@ class VentasExport implements FromCollection, WithHeadings, WithMapping
 
     public function collection()
     {
-        $request = $this->request;//where('id_empresa', Auth::user()->id_empresa)
+        $request = $this->request; //where('id_empresa', Auth::user()->id_empresa)
 
-        $ventas = Venta::when($request->buscador, function($query) use ($request){
-                        return $query->orwhere('correlativo', 'like', '%'.$request->buscador.'%')
-                                    ->orwhere('estado', 'like', '%'.$request->buscador.'%')
-                                    ->orwhere('observaciones', 'like', '%'.$request->buscador.'%')
-                                    ->orwhere('forma_pago', 'like', '%'.$request->buscador.'%');
-                        })
-                        ->when($request->inicio, function($query) use ($request){
-                            return $query->where('fecha', '>=', $request->inicio);
-                        })
-                        ->when($request->recurrente !== null, function($q) use ($request){
-                            $q->where('recurrente', !!$request->recurrente);
-                        })
-                        ->when($request->fin, function($query) use ($request){
-                            return $query->where('fecha', '<=', $request->fin);
-                        })
-                        ->when($request->id_sucursal, function($query) use ($request){
-                            return $query->where('id_sucursal', $request->id_sucursal);
-                        })
-                        ->when($request->id_cliente, function($query) use ($request){
-                            return $query->where('id_cliente', $request->id_cliente);
-                        })
-                        ->when($request->id_usuario, function($query) use ($request){
-                            return $query->where('id_usuario', $request->id_usuario);
-                        })
-                        ->when($request->forma_pago, function($query) use ($request){
-                            return $query->where('forma_pago', $request->forma_pago)
-                                    ->orwhereHas('metodos_de_pago', function($query) use ($request){
-                                        $query->where('nombre', $request->forma_pago);
-                                    });
-                        })
-                        ->when($request->id_vendedor, function($query) use ($request){
-                            return $query->where('id_vendedor', $request->id_vendedor)
-                                    ->orwhereHas('detalles', function($query) use ($request){
-                                        $query->where('id_vendedor', $request->id_vendedor);
-                                    });
-                        })
-                        ->when($request->id_canal, function($query) use ($request){
-                            return $query->where('id_canal', $request->id_canal);
-                        })
-                        ->when($request->id_documento, function($query) use ($request){
-                            return $query->where('id_documento', $request->id_documento);
-                        })
-                        ->when($request->estado, function($query) use ($request){
-                            return $query->where('estado', $request->estado);
-                        })
-                        ->when($request->metodo_pago, function($query) use ($request){
-                            return $query->where('metodo_pago', $request->metodo_pago);
-                        })
-                        ->when($request->tipo_documento, function($query) use ($request){
-                            return $query->where('tipo_documento', $request->tipo_documento);
-                        })
-                        ->where('cotizacion', 0)
-                    ->orderBy($request->orden, $request->direccion)
-                    ->orderBy('id', 'desc')
-                            ->get();
+        $ventas = Venta::when($request->buscador, function ($query) use ($request) {
+            return $query->orwhere('correlativo', 'like', '%' . $request->buscador . '%')
+                ->orwhere('estado', 'like', '%' . $request->buscador . '%')
+                ->orwhere('observaciones', 'like', '%' . $request->buscador . '%')
+                ->orwhere('forma_pago', 'like', '%' . $request->buscador . '%');
+        })
+            ->when($request->inicio, function ($query) use ($request) {
+                return $query->where('fecha', '>=', $request->inicio);
+            })
+            ->when($request->recurrente !== null, function ($q) use ($request) {
+                $q->where('recurrente', !!$request->recurrente);
+            })
+            ->when($request->fin, function ($query) use ($request) {
+                return $query->where('fecha', '<=', $request->fin);
+            })
+            ->when($request->id_sucursal, function ($query) use ($request) {
+                return $query->where('id_sucursal', $request->id_sucursal);
+            })
+            ->when($request->id_cliente, function ($query) use ($request) {
+                return $query->where('id_cliente', $request->id_cliente);
+            })
+            ->when($request->id_usuario, function ($query) use ($request) {
+                return $query->where('id_usuario', $request->id_usuario);
+            })
+            ->when($request->forma_pago, function ($query) use ($request) {
+                return $query->where('forma_pago', $request->forma_pago)
+                    ->orwhereHas('metodos_de_pago', function ($query) use ($request) {
+                        $query->where('nombre', $request->forma_pago);
+                    });
+            })
+            ->when($request->id_vendedor, function ($query) use ($request) {
+                return $query->where('id_vendedor', $request->id_vendedor)
+                    ->orwhereHas('detalles', function ($query) use ($request) {
+                        $query->where('id_vendedor', $request->id_vendedor);
+                    });
+            })
+            ->when($request->id_canal, function ($query) use ($request) {
+                return $query->where('id_canal', $request->id_canal);
+            })
+            ->when($request->id_documento, function ($query) use ($request) {
+                return $query->where('id_documento', $request->id_documento);
+            })
+            ->when($request->estado, function ($query) use ($request) {
+                return $query->where('estado', $request->estado);
+            })
+            ->when($request->metodo_pago, function ($query) use ($request) {
+                return $query->where('metodo_pago', $request->metodo_pago);
+            })
+            ->when($request->tipo_documento, function ($query) use ($request) {
+                return $query->where('tipo_documento', $request->tipo_documento);
+            })
+            ->where('cotizacion', 0)
+            ->orderBy($request->orden, $request->direccion)
+            ->orderBy('id', 'desc')
+            ->get();
 
         return $ventas;
-
     }
 
-    public function map($row): array{
-           $fields = [
-              $row->fecha,
-              $row->nombre_cliente,
-              $row->cliente()->pluck('telefono')->first(),
-              $row->cliente()->pluck('dui')->first(),
-              $row->cliente()->pluck('nit')->first(),
-              $row->cliente()->pluck('direccion')->first(),
-              $row->nombre_documento,
-              $row->correlativo,
-              $row->forma_pago,
-              $row->detalle_banco,
-              $row->estado,
-              $row->nombre_canal,
-              round($row->total_costo, 2),
-              round($row->cuenta_a_terceros, 2),
-              round($row->sub_total, 2),
-              round($row->descuento, 2),
-              round($row->iva, 2),
-              round($row->total - $row->total_costo - $row->iva, 2),
-              round($row->total, 2),
-              $row->sucursal()->first()->empresa()->pluck('nombre')->first(),
-              $row->observaciones,
-              $row->nombre_usuario,
-              $row->nombre_vendedor,
+    public function map($row): array
+    {
+        $fields = [
+            $row->fecha,
+            $row->nombre_cliente,
+            $row->cliente()->pluck('telefono')->first(),
+            $row->cliente()->pluck('dui')->first(),
+            $row->cliente()->pluck('nit')->first(),
+            $row->cliente()->pluck('direccion')->first(),
+            $row->nombre_documento,
+            $row->correlativo,
+            $row->forma_pago,
+            $row->detalle_banco,
+            $row->estado,
+            $row->nombre_canal,
+            $row->estado == 'Anulada' ? '0.0' : round($row->total_costo, 2),
+            round($row->cuenta_a_terceros, 2),
+            $row->estado == 'Anulada' ? '0.0' : round($row->sub_total, 2),
+            $row->estado == 'Anulada' ? '0.0' : round($row->descuento, 2),
+            $row->estado == 'Anulada' ? '0.0' : round($row->iva, 2),
+            $row->estado == 'Anulada' ? '0.0' : round($row->total - $row->total_costo - $row->iva, 2),
+            $row->estado == 'Anulada' ? '0.0' : round($row->total, 2),
+            $row->sucursal()->first()->empresa()->pluck('nombre')->first(),
+            $row->observaciones,
+            $row->nombre_usuario,
+            $row->nombre_vendedor,
 
-         ];
+        ];
         return $fields;
     }
 }
