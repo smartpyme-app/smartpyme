afe3a387da89bcf3d5a90d9493d75fb3e8a8ee7e
David Camacho <dcamacho@growthaccelerationpartners.com>
2025-04-28 21:43:38 -0600

    Actuaizar environment

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://apiconta.smartpyme.site',
-  APP_URL: 'https://contabilidad.smartpyme.site',
+  API_URL: 'https://apicontatest.smartpyme.site',
+  APP_URL: 'https://contabilidadtest.smartpyme.site',
 };
