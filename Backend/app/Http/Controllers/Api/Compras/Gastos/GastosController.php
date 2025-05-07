09cc2617d188dc4e9011b4f8c5538c5245312734
José España <joseespana94@gmail.com>
2025-05-07 11:11:27 -0600

    Agregando campo de area de empresa

--- Backend/app/Http/Controllers/Api/Compras/Gastos/GastosController.php
+++ Backend/app/Http/Controllers/Api/Compras/Gastos/GastosController.php
@@ -123,6 +123,7 @@ class GastosController extends Controller
             'id_usuario'    => 'required|numeric',
             'id_sucursal'   => 'required|numeric',
             'id_empresa'   => 'required|numeric',
+            'area_empresa'   => 'nullable',
         ],[
             'tipo.required' => 'El campo categoria es obligatorio.',
             'id_proveedor.required' => 'El campo proveedor es obligatorio.',
