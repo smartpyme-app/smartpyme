67d17c748f686ea576895d85ace2fced94414939
José España <joseespana94@gmail.com>
2025-05-14 16:03:36 -0600

    Update environment.ts

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:8001',
+  API_URL: 'http://localhost:8000',
   //API_URL: 'https://api.smartpyme.site',
   // API_URL: 'https://apitest.smartpyme.site',
   APP_URL: 'http://localhost:4200',
