ec30696c701de389222ee66ec70ea382a29296f4
Jesús Alvarado <alvarado.websis@gmail.com>
2025-06-16 11:55:25 -0600

    Configuración en empresa de campos para renta

--- Backend/app/Models/Admin/Empresa.php
+++ Backend/app/Models/Admin/Empresa.php
@@ -103,6 +103,11 @@ class Empresa extends Model
         'woocommerce_error',
         'woocommerce_canal_id',
 
+        //Renta
+        'tipo_renta_servicios',
+        'tipo_renta_productos',
+        'tipo_sector',
+
     ];
 
     protected $casts = [
