64c54b48b3121aabbc947417238c9349462451fd
David Camacho <dcamacho@growthaccelerationpartners.com>
2025-06-22 11:34:46 -0600

    Agregar nuevos campso en descarga de excel en ajustes

--- Backend/app/Exports/Inventario/AjustesExport.php
+++ Backend/app/Exports/Inventario/AjustesExport.php
@@ -11,8 +11,8 @@ use App\Models\Inventario\Ajuste;
 class AjustesExport implements FromCollection, WithHeadings, WithMapping
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
 
     private $request;
 
@@ -21,8 +21,9 @@ class AjustesExport implements FromCollection, WithHeadings, WithMapping
         $this->request = $request;
     }
 
-    public function headings():array{
-       return[
+    public function headings(): array
+    {
+        return [
             'Producto',
             'Categoria',
             'Sucursal',
@@ -33,53 +34,60 @@ class AjustesExport implements FromCollection, WithHeadings, WithMapping
             'Fecha',
             'Estado',
             'Motivo',
+            'Costo Unitario',
+            'Diferencia de Ajuste',
         ];
     }
 
-    public function map($row): array{
-           $fields = [
-              $row->producto()->pluck('nombre')->first(),
-              $row->producto()->first() ? $row->producto()->first()->categoria()->pluck('nombre')->first() : '',
-              $row->bodega()->pluck('nombre')->first(),
-              $row->stock_actual,
-              $row->ajuste,
-              $row->stock_real,
-              $row->usuario()->pluck('name')->first(),
-              \Carbon\Carbon::parse($row->created_at)->format('d/m/Y'),
-              $row->estado,
-              $row->concepto,
-         ];
+    public function map($row): array
+    {
+        $diferencia_ajuste = $row->ajuste * ($row->producto()->first() ? $row->producto()->first()->costo : 0);
+
+        $fields = [
+            $row->producto()->pluck('nombre')->first(),
+            $row->producto()->first() ? $row->producto()->first()->categoria()->pluck('nombre')->first() : '',
+            $row->bodega()->pluck('nombre')->first(),
+            $row->stock_actual,
+            $row->ajuste,
+            $row->stock_real,
+            $row->usuario()->pluck('name')->first(),
+            \Carbon\Carbon::parse($row->created_at)->format('d/m/Y'),
+            $row->estado,
+            $row->concepto,
+            $row->producto()->first() ? $row->producto()->first()->costo : 0,
+            $diferencia_ajuste
+        ];
         return $fields;
     }
 
     public function collection()
     {
         $request = $this->request;
-        return Ajuste::when($request->fin, function($query) use ($request){
-                                return $query->whereBetween('created_at', [$request->inicio . ' 00:00:00', $request->fin . ' 23:59:59']);
-                            })
-                            ->when($request->id_bodega, function($query) use ($request){
-                                return $query->whereHas('bodega', function($q) use ($request){
-                                    $q->where('id_bodega', $request->id_bodega);
-                                });
-                            })
-                            ->when($request->id_usuario, function($query) use ($request){
-                                return $query->whereHas('usuario', function($q) use ($request){
-                                    $q->where('id_usuario', $request->id_usuario);
-                                });
-                            })
-                            ->when($request->search, function($query) use ($request){
-                                return $query->whereHas('producto', function($q) use ($request){
-                                    $q->where('nombre', 'like',  '%'. $request->search . '%');
-                                });
-                            })
-                            ->when($request->estado, function($query) use ($request){
-                                $query->where('estado', $request->estado);
-                            })
-                            ->when($request->id_producto, function($query) use ($request){
-                                return $query->where('id_producto', $request->id_producto);
-                            })
-                            ->orderBy($request->orden, $request->direccion)
-                    ->get();
+        return Ajuste::when($request->fin, function ($query) use ($request) {
+            return $query->whereBetween('created_at', [$request->inicio . ' 00:00:00', $request->fin . ' 23:59:59']);
+        })
+            ->when($request->id_bodega, function ($query) use ($request) {
+                return $query->whereHas('bodega', function ($q) use ($request) {
+                    $q->where('id_bodega', $request->id_bodega);
+                });
+            })
+            ->when($request->id_usuario, function ($query) use ($request) {
+                return $query->whereHas('usuario', function ($q) use ($request) {
+                    $q->where('id_usuario', $request->id_usuario);
+                });
+            })
+            ->when($request->search, function ($query) use ($request) {
+                return $query->whereHas('producto', function ($q) use ($request) {
+                    $q->where('nombre', 'like', '%' . $request->search . '%');
+                });
+            })
+            ->when($request->estado, function ($query) use ($request) {
+                $query->where('estado', $request->estado);
+            })
+            ->when($request->id_producto, function ($query) use ($request) {
+                return $query->where('id_producto', $request->id_producto);
+            })
+            ->orderBy($request->orden, $request->direccion)
+            ->get();
     }
 }
