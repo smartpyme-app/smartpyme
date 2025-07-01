f67b8666170099a9291550fef9570ce59cc11adf
Jesús Alvarado <alvarado.websis@gmail.com>
2025-07-01 16:36:24 -0600

    Codigo en DTE

--- Backend/app/Models/MH/MHCCF.php
+++ Backend/app/Models/MH/MHCCF.php
@@ -293,6 +293,12 @@ class MHCCF extends Model
 
             $tributos = NULL;
             $detalle->codTributo = NULL;
+            
+            if ($detalle->producto) {
+                $detalle->codigo = $detalle->producto->codigo;
+            } else {
+                $detalle->codigo = null;
+            }
 
             if ($this->venta->iva > 0) {
                 $tributos = collect();
