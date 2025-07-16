aff5db892d8cb3de64a38f7f333c032f15d93b7a
alexis-garcia-dev <alexis.garcia170698@gmail.com>
2025-07-16 07:32:22 -0600

    Agregar sección de proyecto en la orden de compra

--- Backend/resources/views/reportes/facturacion/orden-de-compra.blade.php
+++ Backend/resources/views/reportes/facturacion/orden-de-compra.blade.php
@@ -87,6 +87,12 @@
                         <p class="text-right">Válido hasta: {{ \Carbon\Carbon::parse($compra->fecha_expiracion)->format('d/m/Y') }}</p>
                     </td>
                 </tr>
+                <tr>
+                    <td><h4>Proyecto</h4></td>
+                </tr>
+                <tr>
+                    <td>{{ $compra->nombre_proyecto ?? 'N/A' }}</td>
+                </tr>
             </tbody>
         </table>
 
