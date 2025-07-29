72fa9ad75c56faeb49bbb4f84df115402e06db4c
David Camacho <dcamacho@growthaccelerationpartners.com>
2025-07-29 11:15:30 -0600

    corregir error routes

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -84,11 +84,11 @@ Route::group(['middleware' => ['jwt.auth']], function () {
 	// require base_path('routes/modulos/empleados/metas.php');
 
 	// Contabilidad
-	// require base_path('routes/modulos/contabilidad/activos.php');
-	// require base_path('routes/modulos/contabilidad/cajas-chicas.php');
-	// require base_path('routes/modulos/contabilidad/presupuestos.php');
-	// require base_path('routes/modulos/contabilidad/proyectos.php');
-	// require base_path('routes/modulos/contabilidad/libros-iva.php');
+	require base_path('routes/modulos/contabilidad/activos.php');
+	require base_path('routes/modulos/contabilidad/cajas-chicas.php');
+	require base_path('routes/modulos/contabilidad/presupuestos.php');
+	require base_path('routes/modulos/contabilidad/proyectos.php');
+	require base_path('routes/modulos/contabilidad/libros-iva.php');
 
 	// Admin
 	require base_path('routes/modulos/admin/empresas.php');
