799c6ec9822a7292d6b467438a7e9b1caf524904
Jesús Alvarado <alvarado.websis@gmail.com>
2025-08-13 14:39:41 -0600

    Agregar campos nuevos en ventas
    Codigo de orden exento en venta
    y codigo de exoneración se usa el NRC

--- Backend/resources/views/reportes/facturacion/formatos_empresas/Factura-Inversiones-Andre.blade.php
+++ Backend/resources/views/reportes/facturacion/formatos_empresas/Factura-Inversiones-Andre.blade.php
@@ -244,7 +244,7 @@
                 <tr>
                     <td>{{ $venta->num_orden }}</td>
                     <td>{{ $venta->num_orden_exento }}</td>
-                    <td></td>
+                    <td>{{ $venta->id_cliente ? $cliente->nrc : '' }}</td>
                     <td></td>
                     <td>{{ \Carbon\Carbon::parse($venta->fecha_pago)->format('d/m/Y') }}</td> 
                     <td>{{ $venta->condicion }}</td> 
