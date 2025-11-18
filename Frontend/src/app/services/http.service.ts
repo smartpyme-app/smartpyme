3e10106909a141bd091ff84a5a1f61714214ed9a
José España <47924337+JOS394@users.noreply.github.com>
2025-11-18 15:18:38 -0600

    Agregando refactorizacion de api service

---
+++ Frontend/src/app/services/http.service.ts
@@ -0,0 +1,213 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { map, catchError, retry, timeout } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { environment } from './../../environments/environment';
+import { AlertService } from '@services/alert.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  public appUrl: string = environment.APP_URL;
+  public baseUrl: string = environment.API_URL;
+  public apiUrl = this.baseUrl + '/api/';
+
+  constructor(
+    private http: HttpClient,
+    private alertService: AlertService
+  ) {}
+
+  private getAuthToken(): string {
+    const token = localStorage.getItem('SP_token');
+    return token ? JSON.parse(token) : '';
+  }
+
+  getToUrl(url: string): Observable<any> {
+    return this.http.get<any>(url).pipe(retry(0), catchError(this.handleError));
+  }
+
+  getAll(url: string, filtros: any = {}): Observable<any> {
+    return this.http
+      .get<any>(this.apiUrl + url, { params: filtros })
+      .pipe(retry(0), catchError(this.handleError));
+  }
+
+  read(url: string, id: number): Observable<any> {
+    return this.http
+      .get<any>(this.apiUrl + url + id)
+      .pipe(retry(0), catchError(this.handleError));
+  }
+
+  filter(url: string, filter: any): Observable<any> {
+    return this.http
+      .get<any>(this.apiUrl + url + filter)
+      .pipe(retry(0), catchError(this.handleError));
+  }
+
+  get(url: string): Observable<any> {
+    return this.http
+      .get<any>(this.apiUrl + url)
+      .pipe(retry(0), catchError(this.handleError));
+  }
+
+  store(url: string, model: any): Observable<any> {
+    return this.http
+      .post<any>(this.apiUrl + url, model)
+      .pipe(retry(0), catchError(this.handleError));
+  }
+
+  storeWithTimeout(url: string, model: any, timeoutMs: number = 300000): Observable<any> {
+    return this.http.post<any>(this.apiUrl + url, model).pipe(
+      timeout(timeoutMs),
+      retry(0),
+      catchError(this.handleError)
+    );
+  }
+
+  update(url: string, id: number, model: any): Observable<any> {
+    return this.http
+      .put<any>(`${this.apiUrl}${url}/${id}`, model)
+      .pipe(retry(0), catchError(this.handleError));
+  }
+
+  delete(url: string, id: number): Observable<any> {
+    return this.http
+      .delete<any>(this.apiUrl + url + id)
+      .pipe(retry(0), catchError(this.handleError));
+  }
+
+  paginate(url: string, filtros: any = {}): Observable<any> {
+    return this.http
+      .get<any>(url, { params: filtros })
+      .pipe(retry(0), catchError(this.handleError));
+  }
+
+  upload(url: string, formData: any): Observable<any> {
+    let headers = new HttpHeaders();
+    headers.append('Accept', 'application/json');
+    const token = this.getAuthToken();
+    if (token) {
+      headers.append('Authorization', 'Bearer ' + token);
+    }
+    let options = { headers };
+    return this.http
+      .post(this.apiUrl + url, formData, options)
+      .pipe(retry(0), catchError(this.handleError));
+  }
+
+  export(url: string, filtros: any): Observable<Blob> {
+    return this.http.get(this.apiUrl + url, { responseType: 'blob', params: filtros });
+  }
+
+  exportWithUrl(url: string, filtros: any): Observable<any> {
+    return this.http.get(this.apiUrl + url, { params: filtros });
+  }
+
+  exportAcumulado(url: string, filtros: any): Observable<Blob> {
+    return this.http.post(this.apiUrl + url, filtros, {
+      responseType: 'blob',
+      headers: new HttpHeaders({
+        'Authorization': 'Bearer ' + this.getAuthToken()
+      }),
+    });
+  }
+
+  exportAcumuladoReportes(url: string, filtros: any): Observable<Blob> {
+    return this.http.post(this.apiUrl + url, filtros, {
+      responseType: 'blob',
+      observe: 'response',
+      headers: new HttpHeaders({
+        'Authorization': 'Bearer ' + this.getAuthToken()
+      })
+    }).pipe(
+      map(response => {
+        return new Blob([response.body!], {
+          type: response.headers.get('Content-Type') || 'application/octet-stream'
+        });
+      })
+    );
+  }
+
+  download(url: string): Observable<Blob> {
+    return this.http.get(`${this.apiUrl}${url}`, {
+      responseType: 'blob',
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + this.getAuthToken()
+      })
+    }).pipe(
+      map((response) => {
+        return new Blob([response]);
+      }),
+      catchError((error) => {
+        console.error('Error al descargar el archivo:', error);
+        return throwError(() => error);
+      })
+    );
+  }
+
+  generatePayrollSlips(planillaId: number): Observable<Blob> {
+    return this.http
+      .get(`${this.apiUrl}planillas/${planillaId}/boletas`, {
+        responseType: 'blob',
+      })
+      .pipe(
+        map((response) => {
+          return new Blob([response], { type: 'application/pdf' });
+        }),
+        catchError((error) => {
+          console.error('Error downloading payroll slips:', error);
+          return throwError(() => error);
+        })
+      );
+  }
+
+  generateIndividualPayrollSlip(detalleId: number): Observable<Blob> {
+    return this.http
+      .get(`${this.apiUrl}planillas/detalles/${detalleId}/boleta`, {
+        responseType: 'blob',
+      })
+      .pipe(
+        map((response) => {
+          return new Blob([response], { type: 'application/pdf' });
+        }),
+        catchError((error) => {
+          console.error('Error downloading payroll slip:', error);
+          return throwError(() => error);
+        })
+      );
+  }
+
+  getUserData(userId: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}me/${userId}`).pipe(
+      map((response: any) => {
+        if (response && response.user) {
+          localStorage.setItem('SP_auth_user', JSON.stringify(response.user));
+          return response.user;
+        }
+        return null;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  getActividadesEconomicas(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}actividades-economicas/excel`).pipe(
+      map((response: any) => {
+        return response;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  getModules(): Observable<any> {
+    return this.http
+      .get<any>(this.apiUrl + 'modules')
+      .pipe(retry(0), catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    return throwError(() => error);
+  }
+}
+
---
+++ Frontend/src/app/services/utility.service.ts
@@ -0,0 +1,244 @@
+import { Injectable } from '@angular/core';
+import * as moment from 'moment';
+import { HttpService } from '@services/http.service';
+import { AlertService } from '@services/alert.service';
+
+declare let $: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilityService {
+  constructor(
+    private httpService: HttpService,
+    private alertService: AlertService
+  ) {}
+
+  saludar(): string {
+    var hours = new Date().getHours();
+    if (hours >= 12 && hours < 18) {
+      return 'Buenas tardes';
+    } else if (hours >= 18) {
+      return 'Buenas noches';
+    } else {
+      return 'Buenos días';
+    }
+  }
+
+  date(): string {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1;
+    let d: string;
+    let m: string;
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+      d = '0' + dd;
+    } else {
+      d = dd.toString();
+    }
+    if (mm < 10) {
+      m = '0' + mm;
+    } else {
+      m = mm.toString();
+    }
+    let date: string = yyyy + '-' + m + '-' + d;
+    return date;
+  }
+
+  datetime(): string {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1;
+    let hh = today.getHours();
+    let min = today.getMinutes();
+    let sec = today.getSeconds();
+    let d: string;
+    let m: string;
+    let h: string;
+    let se: string;
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+      d = '0' + dd;
+    } else {
+      d = dd.toString();
+    }
+    if (mm < 10) {
+      m = '0' + mm;
+    } else {
+      m = mm.toString();
+    }
+    if (sec < 10) {
+      se = '0' + sec;
+    } else {
+      se = sec.toString();
+    }
+    let datetime: string =
+      yyyy + '-' + m + '-' + d + ' ' + hh + ':' + min + ':' + se;
+    return datetime;
+  }
+
+  dataURItoBlob(dataURI: any): Blob {
+    let byteString: any;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0) {
+      byteString = atob(dataURI.split(',')[1]);
+    } else {
+      byteString = unescape(dataURI.split(',')[1]);
+    }
+    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    const ia = new Uint8Array(byteString.length);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ia], { type: mimeString });
+  }
+
+  slug(str: any): string | undefined {
+    if (str) {
+      str = str.replace(/^\s+|\s+$/g, '');
+      str = str.toLowerCase();
+      var from = 'àáäâèéëêìíïîòóöôùúüûñç·/_,:;';
+      var to = 'aaaaeeeeiiiioooouuuunc------';
+      for (var i = 0, l = from.length; i < l; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+      }
+      str = str
+        .replace(/[^a-z0-9 -]/g, '')
+        .replace(/\s+/g, '-')
+        .replace(/-+/g, '-');
+      return str;
+    }
+    return undefined;
+  }
+
+  toggleTheme(): void {
+    if (localStorage.getItem('SP_theme') == 'light') {
+      localStorage.setItem('SP_theme', 'dark');
+    } else {
+      localStorage.setItem('SP_theme', 'light');
+    }
+    this.loadTheme();
+  }
+
+  loadTheme(): void {
+    let theme: any = localStorage.getItem('SP_theme');
+    if (!theme) {
+      localStorage.setItem('SP_theme', 'light');
+    }
+    if (localStorage.getItem('SP_theme') == 'dark') {
+      $('body').attr('data-theme-version', 'dark');
+      $('.icon-theme').removeClass('far');
+      $('.icon-theme').addClass('fas');
+    } else {
+      $('body').attr('data-theme-version', 'light');
+      $('.icon-theme').removeClass('fas');
+      $('.icon-theme').addClass('far');
+    }
+  }
+
+  downloadFile(blob: Blob, filename: string): void {
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = filename;
+    link.click();
+    window.URL.revokeObjectURL(url);
+  }
+
+  generateGoogleCalendarLink(event: any): string {
+    const startDate = moment(event.startDate)
+      .utc()
+      .format('YYYYMMDDTHHmmss[Z]');
+    const endDate = moment(event.endDate).utc().format('YYYYMMDDTHHmmss[Z]');
+    const calendarLink = `https://www.google.com/calendar/event?action=TEMPLATE&dates=${startDate}/${endDate}&text=${encodeURIComponent(
+      event.title
+    )}&details=${encodeURIComponent(
+      event.description
+    )}&location=${encodeURIComponent(event.location)}`;
+    return calendarLink;
+  }
+
+  getPosition(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        (resp) => {
+          resolve({ lng: resp.coords.longitude, lat: resp.coords.latitude });
+        },
+        (err) => {
+          reject(err);
+        }
+      );
+    });
+  }
+
+  loadData(): void {
+    this.httpService.getAll('formas-de-pago').subscribe(
+      (metodospago: any) => {
+        localStorage.setItem('metodospago', JSON.stringify(metodospago));
+      },
+      (error: any) => {
+        this.alertService.error(error);
+      }
+    );
+
+    this.httpService.getAll('paises').subscribe(
+      (paises: any) => {
+        localStorage.setItem('paises', JSON.stringify(paises));
+      },
+      (error: any) => {
+        this.alertService.error(error);
+      }
+    );
+
+    this.httpService.getAll('municipios').subscribe(
+      (municipios: any) => {
+        localStorage.setItem('municipios', JSON.stringify(municipios));
+      },
+      (error: any) => {
+        this.alertService.error(error);
+      }
+    );
+
+    this.httpService.getAll('distritos').subscribe(
+      (distritos: any) => {
+        localStorage.setItem('distritos', JSON.stringify(distritos));
+      },
+      (error: any) => {
+        this.alertService.error(error);
+      }
+    );
+
+    this.httpService.getAll('departamentos').subscribe(
+      (departamentos: any) => {
+        localStorage.setItem('departamentos', JSON.stringify(departamentos));
+      },
+      (error: any) => {
+        this.alertService.error(error);
+      }
+    );
+
+    this.httpService.getAll('actividades_economicas').subscribe(
+      (actividad_economicas: any) => {
+        localStorage.setItem('actividad_economicas', JSON.stringify(actividad_economicas));
+      },
+      (error: any) => {
+        this.alertService.error(error);
+      }
+    );
+
+    this.httpService.getAll('unidades').subscribe(
+      (medidas: any) => {
+        localStorage.setItem('unidades_medidas', JSON.stringify(medidas));
+      },
+      (error: any) => {
+        this.alertService.error(error);
+      }
+    );
+  }
+
+  getConstants(): any {
+    const constants = localStorage.getItem('SP_constants');
+    return constants ? JSON.parse(constants) : null;
+  }
+}
+
