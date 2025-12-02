32541b8f1ed371470ee26e6705994050c5492dce
Jesús Alvarado <alvarado.websis@gmail.com>
2025-12-02 09:37:37 -0600

    Eager loading en compras y ventas

--- Backend/app/Http/Controllers/Api/Compras/ComprasController.php
+++ Backend/app/Http/Controllers/Api/Compras/ComprasController.php
@@ -112,6 +112,7 @@ class ComprasController extends Controller
             ->withSum(['devoluciones' => function ($query) {
                 $query->where('enable', 1);
             }], 'total')
+            ->withAccessorRelations()
             ->orderBy($request->orden, $request->direccion)
             ->orderBy('id', 'desc')
             ->paginate($request->paginate);
