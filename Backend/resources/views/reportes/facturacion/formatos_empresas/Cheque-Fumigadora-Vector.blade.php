01e53c2fa970fd6510170353e2b20b3f059974cb
Jesús Alvarado <alvarado.websis@gmail.com>
2025-12-10 14:04:58 -0600

    Ajuste formato cheche vector

--- Backend/resources/views/reportes/facturacion/formatos_empresas/Cheque-Fumigadora-Vector.blade.php
+++ Backend/resources/views/reportes/facturacion/formatos_empresas/Cheque-Fumigadora-Vector.blade.php
@@ -21,7 +21,7 @@
         #anio          {top: 2cm; left: 12cm; width: 2cm;}
         #anombrede      {top: 3cm; left: 4.2cm; width: 14cm; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;}
         #nombre_cuenta  {top: 4.5cm; left: 2.7cm; width: 9cm;}
-        #concepto       {top: 6.5cm; left: 4.5cm; }
+        /* #concepto       {top: 6.5cm; left: 4.5cm; } */
         #total          {top: 2cm; left: 14.5cm; }
         #letras         {top: 3.8cm; left: 3.8cm; width: 14cm; word-break: break-all; white-space: normal;}
         
@@ -38,11 +38,12 @@
 <section id="factura">
     <div id="header">
 
-        <p id="lugarfecha">{{ \Carbon\Carbon::now()->format('d/m/Y') }}</p>
+        <p id="lugarfecha">
+            {{ $cheque->empresa->municipio }}, {{ $cheque->empresa->departamento }}, {{ \Carbon\Carbon::now()->format('d') }} de {{ \Carbon\Carbon::now()->translatedFormat('F') }}
+        </p>
         <p id="anio">{{ \Carbon\Carbon::parse($cheque->fecha)->format('Y') }}</p>
         <p id="anombrede">{{ $cheque->anombrede }}</p>
-        {{-- <p id="nombre_cuenta">{{ $cheque->nombre_cuenta }}</p> --}}
-        <p id="concepto">{{ $cheque->concepto }}</p>
+        <!-- <p id="concepto">{{ $cheque->concepto }}</p> -->
         <p id="total">{{ $cheque->total }}</p>
 
         <p id="letras"> {{$dolares}} DÓLARES CON {{$centavos}} CENTAVOS.</p>
