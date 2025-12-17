afb92438fec51490ab582788a076f0a42da43888
José España <245966543+j-espana@users.noreply.github.com>
2025-12-17 10:20:54 -0600

    Agregando campo vendedor asignado

--- Backend/routes/modulos/super-admin/usuarios.php
+++ Backend/routes/modulos/super-admin/usuarios.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\SuperAdmin\UsuariosController;
 
     Route::get('/admin-usuarios',                 [UsuariosController::class, 'index']);
+    Route::get('/admin-usuarios/list-vendedores', [UsuariosController::class, 'listVendedores']);
     Route::get('/admin-usuarios/list',            [UsuariosController::class, 'list']);
     Route::post('/admin-usuario',                 [UsuariosController::class, 'store']);
     Route::get('/admin-usuario/{id}',             [UsuariosController::class, 'read']);
