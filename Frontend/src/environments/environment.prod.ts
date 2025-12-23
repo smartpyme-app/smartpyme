b402c99dc5f4af6539e2b3bcf59ccdd2b59ab1d9
sebasCast1996 <105011615+sebasCast1996@users.noreply.github.com>
2025-12-23 12:07:42 -0600

    Removed typo

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   //API_URL: 'https://apitest.smartpyme.site',
-  API_URL: 'https://v5uvwsmui8.us-east-2.awsapprunner.com/',
+  API_URL: 'https://v5uvwsmui8.us-east-2.awsapprunner.com',
   APP_URL: 'https://app.smartpyme.site',
 };
