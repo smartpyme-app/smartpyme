fc475e39096f63dcad12c036990eeaffa9611948
sebasCast1996 <105011615+sebasCast1996@users.noreply.github.com>
2026-02-11 21:47:05 -0600

    Backend URLs

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  //API_URL: 'https://apitest.smartpyme.site',
-  API_URL: 'https://api.smartpyme.site',
-  APP_URL: 'https://app.smartpyme.site',
+  API_URL: 'https://2arwsygm3n.us-east-2.awsapprunner.com',
+  APP_URL: 'https://d37ls7cx9pvmle.cloudfront.net',
 };
