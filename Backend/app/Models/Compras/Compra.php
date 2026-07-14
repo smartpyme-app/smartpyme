0fef8c66151cf292bd3feb5cdf78d0391fa6811f
Jesús Alvarado <alvarado.websis@gmail.com>
2026-07-14 17:05:03 -0600

    Update Compra.php

--- Backend/app/Models/Compras/Compra.php
+++ Backend/app/Models/Compras/Compra.php
@@ -150,10 +150,6 @@ class Compra extends AuditableModel {
         return $this->hasMany('App\Models\Compras\Devoluciones\Devolucion', 'id_compra');
     }
 
-    public function impuestos(){
-        return $this->hasMany('App\Models\Compras\Impuesto', 'id_compra');
-    }
-
     /**
      * Retaceo vinculado (una compra solo puede estar en un retaceo).
      */
