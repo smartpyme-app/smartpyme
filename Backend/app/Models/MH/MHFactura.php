90848df571b464d353e9e17d4aa0918301fd02a8
Jesús Alvarado <alvarado.websis@gmail.com>
2026-08-14 15:19:25 -0600

    Update MHFactura.php

--- Backend/app/Models/MH/MHFactura.php
+++ Backend/app/Models/MH/MHFactura.php
@@ -354,8 +354,11 @@ class MHFactura extends Model
 
             if ($this->documentoTieneIva()) {
                 $factor = $this->factorIvaIncluidoDetalle($detalle);
-                $detalle->precio = round($detalle->precio * $factor, 4);
-                $detalle->descuento = round($detalle->descuento * $factor, 2);
+                $precioConIva = (float) ($detalle->precio_con_iva ?? 0);
+                $detalle->precio = $precioConIva > 0
+                    ? round($precioConIva, 4)
+                    : round((float) $detalle->precio * $factor, 4);
+                $detalle->descuento = round((float) $detalle->descuento * $factor, 2);
                 $detalle->gravada = ($detalle->cantidad * $detalle->precio) - $detalle->descuento;
                 $tributos = $this->buildTributosLineaCodesFacturaConsumidor($detalle);
             } else {
