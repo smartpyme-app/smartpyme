64f42de0c9b955919746ee1ef250d8b072528bee
Jesús Alvarado <alvarado.websis@gmail.com>
2026-09-02 14:02:19 -0600

    Update cotizacion-detalle-descripcion.blade.php

--- Backend/resources/views/reportes/facturacion/partials/cotizacion-detalle-descripcion.blade.php
+++ Backend/resources/views/reportes/facturacion/partials/cotizacion-detalle-descripcion.blade.php
@@ -85,7 +85,7 @@
             </td>
             <td style="vertical-align: top; border: none; padding: 0;">
                 <div style="font-weight: 700; text-transform: uppercase; font-size: 11px; line-height: 1.3; margin-bottom: 4px;">
-                {{ $detalle->nombre_producto }}
+                {{ $detalle->descripcion }}
                 </div>
                 @if($variacionLinea !== '')
                     <div style="font-size: 10px; line-height: 1.35; margin-bottom: 6px; color: #222;">
